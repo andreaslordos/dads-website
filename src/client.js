ceb80baa10154b3985b58eda6a7d4734aa6d44a0
Andreas Lordos <andreasglordos@gmail.com>
2023-08-25 17:52:42 +0300

    switched to using cdn to cache website

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ import {createClient} from '@sanity/client'
 export const sanityClient = createClient({
   projectId: 'ynmpcv7c',
   dataset: 'production',
-  useCdn: false, // set to `false` to bypass the edge cache
+  useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2023-08-24', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 })
